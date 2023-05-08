8af777aa11b5d46832579ea8a45af97d88aaa9a1
Alex <alexandre.legleye@gmail.com>
2023-05-08 20:15:17 +0200

    init repo

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import psql from "./tech/psql.png";
+import recoil from "./tech/recoil.svg";
+
+import meta from "./company/meta.png";
+
+import idfrog from "./idfrog.png";
+import weathermap from "./weathermap.png";
+import cospi from "./cospi.png";
+import portfolio from "./portfolio.png";
+
+
+import oclock from "./company/logo-oclock.svg/";
+import psa from "./company/PSA_logo.svg"
+import poleemploi from "./company/pole_emploi.jpg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  psql,
+  cospi,
+  weathermap,
+  idfrog,
+  portfolio,
+  oclock,
+  psa,
+  poleemploi,
+  recoil
+};
---
+++ src/constants/index.js
@@ -0,0 +1,235 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  nodejs,
+  psql,
+  cospi,
+  weathermap,
+  idfrog,
+  portfolio,
+  oclock,
+  psa,
+  poleemploi,
+  github,
+  recoil
+} from "../assets";
+  
+export const navLinks = [
+  {
+    id: "about",
+    title: "A propos",
+  },
+  {
+    id: "work",
+    title: "Expériences",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+  
+const services = [
+  {
+    title: "Développeur Web Fullstack",
+    icon: web,
+  },
+  {
+    title: "Développeur Front-end",
+    icon: mobile,
+  },
+  {
+    title: "Développeur Back-end",
+    icon: backend,
+  },
+  {
+    title: "Base de données",
+    icon: creator,
+  },
+];
+  
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "PSQL",
+    icon: psql,
+  },
+  {
+    name: "github",
+    icon: github,
+  },
+  {
+    name:"recoil",
+    icon:recoil,
+  },
+];
+  
+const experiences = [
+  {
+    title: "TP : Développeur Web et Web Mobile",
+    company_name: "O'Clock",
+    icon: oclock,
+    iconBg: "#383E56",
+    date: "Mai 2022 - Novembre 2022",
+    points: [
+      "798 heures intensives (téléprésentiel)",
+      "3 mois de socle (HTML5, CSS3, Javascript, NodeJs, PSQL)",
+      "1 mois de spécialisation en ReactJs",
+      "1 mois de projet professionnalisant",
+    ],
+  },
+  {
+    title: "Conseiller à l'emploi",
+    company_name: "Pôle Emploi",
+    icon: poleemploi,
+    iconBg: "#FFF",
+    date: "Octobre 2018 - Avril 2022",
+    points: [
+      "Création d’outil de gestion et de suivi des évènements dans l’agence, création d’un outil de suivi des demandeurs en attente de formation",
+      "Organisation et animation d’informations collectives et d’ateliers",
+      "Gestion d'un portefeuille de demandeurs d'emplois",
+    ],
+  },
+  {
+    title: "Conception moteur",
+    company_name: "PSA Groupe",
+    icon: psa,
+    iconBg: "#E6DEDD",
+    date: "Septembre 2007 - Septembre 2018",
+    points: [
+      "Planification de programme d’essai en interne et en externe (Allemagne, Italie)",
+      "relation avec les fournisseurs et sous -traitants.",
+      "Suivi des essais et des moyens d’essai, compte rendu de campagne d’essai",
+      "Formation des nouveaux arrivants aux procédures et logiciels d’essais"
+    ],
+  },
+];
+  
+const testimonials = [
+  {
+    testimonial:
+        "Le jury t’a trouvé posé. Tu as démontré une bonne maîtrise du sujet aussi bien en front qu’en back. Ta bonne aisance communicationnelle a été appréciée.",
+    name: "Titre PRO ",
+    designation: "Jury",
+    company: "DWWM",
+    image: html,
+  },
+  {
+    testimonial:
+        "Tu as de réelles aptitudes pour ce métier, c’est indéniable ! Tu auras fourni un travail régulier tout au long de la formation, même dans les cours les plus difficiles. Ton investissement dans l’apothéose, pendant laquelle tu as montré de belles compétences, nous prouve que tu peux avoir confiance en toi ! Continue sur cette lancée, continue à pratiquer, tu as un avenir certain dans le développement 😉",
+    name: "Andre L.",
+    designation: "Référent pédagogique",
+    company: "O'Clock",
+    image: oclock,
+  },
+];
+  
+const projects = [
+  {
+    name: "Portfolio",
+    description:
+        "Mon portfolio, préentant mon parcours et regroupant mes projets disponibles",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "text-blue-200",
+      },
+    ],
+    image: portfolio,
+    source_code_link: "https://github.com/",
+    deploy_link: "https://alexandre-legleye.com/",
+  },
+  {
+    name: "Id Frog",
+    description:
+        "Plateforme de mise en relation des porteurs de projets et des investisseurs (business angels)",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "text-yellow-200",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "psql",
+        color: "grey-text-gradient",
+      },
+      {
+        name: "Material UI",
+        color: "text-blue-700",
+      },
+      {
+        name: "Recoil",
+        color: "text-blue-500",
+      },
+    ],
+    image: idfrog,
+    source_code_link: "https://github.com/alexandrelegleye/IdFrog-Legleye",
+    deploy_link: "https://idfrog.alexandre-legleye.com/",
+  },
+  {
+    name: "Cospi athlétisme",
+    description:
+        "Site de présentation et d'inscription pour un club d'athlétisme de la Réunion",
+    tags: [
+      {
+        name: "WordPress",
+        color: "white-text-gradient",
+      },
+    ],
+    image: cospi,
+    //source_code_link: "https://github.com/",
+    deploy_link: "https://cospi.re/",
+  },
+  {
+    name: "Appli Météo",
+    description:
+        "Application météo codée au début de ma formation React",
+    tags: [
+      {
+        name: "React",
+        color: "white-text-gradient",
+      },
+    ],
+    image: weathermap,
+    source_code_link: "https://github.com/alexandrelegleye/Meteo-with-map",
+    deploy_link: "https://weather-map.alexandre-legleye.com/",
+  },
+];
+  
+export { services, technologies, experiences, testimonials, projects };
